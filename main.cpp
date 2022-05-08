a400703ab29a5d7157da425cfbbc646f3ce0cc66
FrancescoPittaluga02 <100779816+FrancescoPittaluga02@users.noreply.github.com>
2022-05-08 17:42:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int main(){
+int main() {
   char a;
-  cin>>a;
-  bool risultato=primo(a);
-  if (risultato) {
+  cin >> a;
+  bool risultato = primo(a);
+  if (risultato) {   
     cout << trasformazione(a);
-  }else{
-    cout<<"errore"<< endl;
+  } else { 
+    cout << "errore" << endl;
   }
     return 0;
 }
